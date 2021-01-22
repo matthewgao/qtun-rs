1fea7edf6739e37621f4058f5faca4c7c0c3ee4b
gaoshen.gs <gaoshen.gs@alibaba-inc.com>
2021-01-23 00:30:22 +0800

    protobuf

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,10 @@ mod rpc;
 mod tun;
 mod encrypt;
 mod config;
+mod connection;
 
 use config::opt;
+use connection::message;
 //from_args is a trait from StructOpt, if you want to use in different module, than you need to "use" it first -gs
 // use structopt::StructOpt;
 
@@ -15,4 +17,9 @@ fn main() {
 
     let opt = opt::get_config();
     println!("{:#?}", opt);
+
+    let mut msg = message::MessagePing::new();
+    msg.Timestamp = 10000;
+
+    println!("{:#?}", msg)
 }
